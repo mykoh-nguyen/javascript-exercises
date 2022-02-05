4db846cc87423bceefca36170a8116b6d5c50ae6
Michael Nguyen <mykoh93@gmail.com>
2022-02-04 17:01:33 -0800

    repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,amount) {
+    if(amount < 0){
+        return "ERROR";
+    }
+    let newString = "";
+    for(let i = 0; i < amount; i++){
+        newString +=string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
