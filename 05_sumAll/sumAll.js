c251900c891822a441c425843ab423bab0241dab
Michael Nguyen <mykoh93@gmail.com>
2022-02-07 17:16:43 -0800

    SumAll Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    var sum = 0;
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    if(typeof num1 && typeof num2 != "number"){
+        return "ERROR";
+    }
+    if(num1 < num2){
+        for(var i = num1; i <= num2;i++){
+            sum+=i
+        }
+    }
+    else if(num1 >num2){
+        for(var x = num2; x <= num1;x++){
+            sum+=x
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+
