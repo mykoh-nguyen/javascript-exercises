1ae29728608d0d122a7546a8d5b173a0eb5c1fd8
Michael Nguyen <mykoh93@gmail.com>
2022-02-09 14:28:38 -0800

    leapYears completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
 const leapYears = function() {
-
+    if(arguments[0] % 4 != 0){
+        return false;
+    }
+    if(arguments[0] % 100 == 0){
+        if(arguments[0] % 4 == 0 && arguments[0] % 400 ==0 ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else return true;
 };
 
 // Do not edit below this line
