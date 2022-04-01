475a10d26c35c96e8a53c454558d0ebb56177c36
Sang Huynh <mykoh93@gmail.com>
2022-03-31 23:16:57 -0700

    fibonacci finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if(a < 0){
+        return "OOPS"
+    }
+    let newArr = [];
+
+    for(i = 0 ; i <a ; i++){
+        if(i ==0){
+            newArr.push(1)
+        }
+        else if(newArr[i-2] == null){
+            newArr.push(newArr[i-1]);
+        }
+        else{
+            newArr.push(newArr[i-1] + newArr[i-2]);
+        }
+    }
+    return newArr[a-1];
 
 };
 
