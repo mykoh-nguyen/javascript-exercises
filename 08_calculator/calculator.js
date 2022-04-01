856fab0c8c0b20599305bd813c485e9e5ff1f2d2
Sang Huynh <mykoh93@gmail.com>
2022-03-31 22:59:15 -0700

    finished palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return (a > b ? a- b : b -a)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  for(i =0 ; i< arr.length; i++){
+    total+= arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let total = 1;
+
+  for(i = 0 ;i<arr.length ;i++){
+    total*= arr[i];
+  }
+  return total;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+
+  while(a != 0){
+    total = total * a;
+    a--;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let backwards = "";
+    let newString = ""
+    for(i = string.length-1 ;i >= 0; i--){
+        if(!!string[i].match(/[.,;!?\s]/) == true){
+            continue;
+        }
+        else{
+        backwards+=string[i].toLowerCase();
+        }
+    }
+    for( x = 0 ; x < string.length;x++){
+        if(!!string[x].match(/[.,;!?\s]/) == true){
+            continue;
+        }
+        else{
+        newString+=string[x].toLowerCase();
+        }
+    }
+    return(newString == backwards ? true : false);
 
 };
 
